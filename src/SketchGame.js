56b303ab2d3cc0cbb0284f91b038ec3905268150
tom880523 <tom880523@gmail.com>
2026-02-09 01:55:14 +0800

    手機介面優化5

--- src/SketchGame.js
+++ src/SketchGame.js
@@ -1016,8 +1016,8 @@ function SketchGameInterface({ roomData, isHost, roomId, currentUser, getCurrent
                             )}
 
                             {/* Canvas Container */}
-                            {/* Mobile Portrait: aspect-video | Mobile Landscape: h-[60svh] fixed | Desktop: min-h-500 */}
-                            <div className="border-2 border-slate-600 rounded-xl overflow-hidden bg-white w-full aspect-video relative touch-none landscape:flex-1 landscape:aspect-auto landscape:h-[60svh] landscape:max-h-[60svh] md:!aspect-video md:!h-auto md:!max-h-none md:!min-h-[500px] md:!flex-none">
+                            {/* Always aspect-video (16:9) | Portrait: max-h-50svh | Landscape: max-h-70svh + max-w | Desktop: min-h-500 */}
+                            <div className="border-2 border-slate-600 rounded-xl overflow-hidden bg-white w-full max-h-[50svh] aspect-video relative touch-none landscape:max-h-[70svh] landscape:w-auto landscape:max-w-[calc(100vw-8rem)] md:!aspect-video md:!h-auto md:!max-h-none md:!min-h-[500px] md:!w-full md:!max-w-none md:!flex-none">
                                 {isDrawer ? (
                                     <ReactSketchCanvas
                                         ref={canvasRef}
