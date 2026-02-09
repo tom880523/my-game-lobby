37971956b30603588d86fa03bdc7170aaac1429b
tom880523 <tom880523@gmail.com>
2026-02-10 00:09:41 +0800

    手機介面優化1

--- src/SketchGame.js
+++ src/SketchGame.js
@@ -1016,8 +1016,8 @@ function SketchGameInterface({ roomData, isHost, roomId, currentUser, getCurrent
                             )}
 
                             {/* Canvas Container */}
-                            {/* Portrait: w-[95vw] + 4:3 | Landscape: height-based (100vh-140px) + 16:9 to fit screen without scrolling | Desktop: min-h-500 */}
-                            <div className="border-2 border-slate-600 rounded-xl overflow-hidden bg-white w-[95vw] aspect-[4/3] mx-auto relative touch-none landscape:w-auto landscape:h-[calc(100vh-140px)] landscape:mx-0 landscape:aspect-video md:!h-auto md:!max-h-none md:!max-w-none md:!min-h-[500px] md:!w-full md:!mx-0 md:!aspect-video">
+                            {/* Portrait: w-[95vw] + 4:3 | Landscape: strict max-w + max-h to guarantee fit | Desktop: min-h-500 */}
+                            <div className="border-2 border-slate-600 rounded-xl overflow-hidden bg-white w-[95vw] aspect-[4/3] mx-auto relative touch-none landscape:w-auto landscape:h-auto landscape:max-h-[calc(100vh-160px)] landscape:max-w-[calc((100vh-160px)*16/9)] landscape:mx-0 landscape:aspect-video md:!h-auto md:!max-h-none md:!max-w-none md:!min-h-[500px] md:!w-full md:!mx-0 md:!aspect-video">
                                 {isDrawer ? (
                                     <ReactSketchCanvas
                                         ref={canvasRef}
